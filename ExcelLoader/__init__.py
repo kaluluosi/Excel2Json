f0a77e72d3be8c0e9eda93c7060afda6a3bbda90
kaluluosi111 <kaluluosi@gmail.com>
2019-10-19 17:22:16 +0800

    清理 文件

--- ExcelLoader/__init__.py
+++ ExcelLoader/__init__.py
@@ -2,13 +2,3 @@
 from .Loader import create_loader
 from .Generator import Generator
 
-
-import sys
-import os
-
-if __name__ == '__main__':
-
-    excel_file = sys.argv[1]
-    _dir = os.path.splitext(excel_file)[0]
-    gen = Generator(sys.argv[1], os.path.join(_dir, 'config.json'))
-
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Excel2Json',
+      version='1.0',
+      description='Excel转json导表工具',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
